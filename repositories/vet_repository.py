8d8e6914e06dd9bff0a0aad06f18008e66614284
Danny <carey.d@ntlworld.com>
2023-08-26 14:44:40 +0100

    added new subfolders in the templates folder. added view all fuction for vet_repo. added content to vets.html to display vets, delete a vet and the form to add a new vet to the database. added the classes to the stylesheet.

--- repositories/vet_repository.py
+++ repositories/vet_repository.py
@@ -3,3 +3,17 @@ from db.run_sql import run_sql
 from models.pet import Pet 
 from models.vet import Vet 
 
+def vets_view_all():
+    vets = []
+    sql = "SELECT * FROM vets"
+    results = run_sql(sql)
+
+    for row in results:
+        vet = Vet(row['name'], row['id'])
+        vets.append(vet)
+    return vets
+
+#vets_view_all() requests all of the information on the vets table on the database and returns it into the "vets" list.
+
+def vet_add():
+    return None
