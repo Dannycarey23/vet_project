c30259dcf3c7e31b6b93f131913481622ea4858b
Danny <carey.d@ntlworld.com>
2023-08-26 21:55:39 +0100

    routes added for vet controller; view all vets, add a new vet and delete a vet. Associated methods in the vet repository also added.

--- controllers/vet_controller.py
+++ controllers/vet_controller.py
@@ -4,3 +4,21 @@ from models.vet import Vet
 import repositories.vet_repository as vet_repository
 import repositories.pet_repository as pet_repository
 
+vets_blueprint = Blueprint("vets", __name__)
+
+@vets_blueprint.route("/vets")
+def vets():
+    vets = vet_repository.vets_view_all()
+    return render_template("vets/index.html", vets=vets)
+
+@vets_blueprint.route("/newvet", methods=['POST'])
+def new_vet():
+    name = request.form['name']
+    vet = Vet(name)
+    vet_repository.vet_add(vet)
+    return redirect('/vets/index.html')
+
+@vets_blueprint.route("/vets/<id>/delete", methods=['POST'])
+def delete_a_vet(id):
+    vet_repository.vet_delete(id)
+    return redirect('/books')
